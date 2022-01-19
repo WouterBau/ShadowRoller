46ed8bb2f35511ff9a61f54b0d0731e4afa5b592
Wouter Bauchart <wouter.bauchart@outlook.com>
2022-01-19 03:01:47 +0100

    Added first steps of interpreting commands

--- ShadowRoller.DiscordBot/DiscordBot.cs
+++ ShadowRoller.DiscordBot/DiscordBot.cs
@@ -4,6 +4,8 @@ using DSharpPlus.EventArgs;
 public class DiscordBot
 {
 
+    private const string PREFIX = "!sr-";
+    private const string DELIMITER = " ";
     private readonly CancellationTokenSource _cancellationTokenSource;
 
     public DiscordBot(DiscordClient discordClient, CancellationTokenSource cancellationTokenSource)
@@ -14,6 +16,23 @@ public class DiscordBot
 
     private async Task OnMessageCreated(DiscordClient sender, MessageCreateEventArgs e)
     {
-        throw new NotImplementedException();
+        if(!e.Message.Content.StartsWith(PREFIX, StringComparison.InvariantCultureIgnoreCase))
+            return;
+        
+        var messageParts = e.Message.Content.Split(DELIMITER).ToArray();
+        var command = messageParts.First().Replace(PREFIX, "").ToLower();
+
+        switch(command)
+        {
+            case "exit":
+                await sender.SendMessageAsync(e.Message.Channel, "Ok, I'm leaving");
+                _cancellationTokenSource.Cancel();
+                break;
+            case "eval":
+                break;
+            case "roll":
+                break;
+        }
+
     }
 }
