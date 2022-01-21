8a4d743ede4f88f5c34fd2043e43203b3eeb5277
WouterB <wouter.bauchart@outlook.com>
2022-01-21 03:14:18 +0100

    Initial creation of Discord bot for ShadowRun edition 5 (#1)
    * Initial bot setup
    
    * Added first steps of interpreting commands
    
    * Added domain logic for evaluating a roll result
    
    * Added simple random dice rolling
    
    * Implemented calls to logic from bot
    
    * Code cleanup
    
    * Bugfixes after testing

---
+++ ShadowRoller.DiscordBot/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Hello, World! Starting this bot!");
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets(typeof(Program).Assembly, true)
+    .Build();
+
+var cancellationTokenSource = new CancellationTokenSource();
+
+var discordClient = new DiscordClient(
+    new DiscordConfiguration
+    {
+        AutoReconnect = true,
+        MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+        Token = config["discord:token"],
+        TokenType = TokenType.Bot
+    }
+);
+
+var discordBot = new DiscordBot(discordClient, cancellationTokenSource);
+
+await discordClient.ConnectAsync();
+
+while(!cancellationTokenSource.IsCancellationRequested)
+{
+
+}
---
+++ ShadowRoller.Domain/ShadowRunRollResult.cs
@@ -0,0 +1,29 @@
+namespace ShadowRoller.Domain;
+public class ShadowRunRollResult
+{
+    private IEnumerable<int> _diceResults = new int[]{};
+    public IEnumerable<int> DiceResults
+    {
+        get => _diceResults;
+        init
+        {
+            if (value == null)
+                return;
+            _diceResults = value.Where(x => 1 <= x && x <= 6);
+        }
+    }
+    public int? HitLimit { get; init; }
+    public int GrossAmountHits => DiceResults.Where(x => x >= 5).Count();
+    public int NetAmountHits
+    {
+        get
+        {
+            if (HitLimit.HasValue && HitLimit.Value < GrossAmountHits)
+                return HitLimit.Value;
+            return GrossAmountHits;
+        }
+    }
+    public int AmountMisses => DiceResults.Where(x => x == 1).Count();
+    public bool HasGlitched => AmountMisses > (DiceResults.Count() / 2);
+    public bool HasGlitchedCritically => HasGlitched && GrossAmountHits == 0;
+}
