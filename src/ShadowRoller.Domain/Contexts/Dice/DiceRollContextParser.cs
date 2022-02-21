109e0fd318f76e9f819c65cce2bf0a41a3a88f8f
Wouter Bauchart <wouter.bauchart@outlook.com>
2022-02-21 03:03:18 +0100

    Implemented context parser for die roll

--- src/ShadowRoller.Domain/Contexts/Dice/DiceRollContextParser.cs
+++ src/ShadowRoller.Domain/Contexts/Dice/DiceRollContextParser.cs
@@ -1,8 +1,25 @@
+using System.Text.RegularExpressions;
+
 namespace ShadowRoller.Domain.Contexts.Dice;
 public class DiceRollContextParser : IRollContextParser<DiceRollContext, DiceModifierSumRollResult>
 {
+    private readonly Regex _regex = new Regex("^\\d\\d*d\\d\\d*$");
     public DiceRollContext ParseToRollContext(string[] arguments)
     {
-        throw new NotImplementedException();
+        var dice = new List<Die>();
+        var modifiers = new List<int>();
+        foreach (var arg in arguments.Select(x => x.Trim()))
+        {
+            if (int.TryParse(arg, out var modifier))
+                modifiers.Add(modifier);
+            else if (_regex.IsMatch(arg))
+            {
+                var values = arg.Split('d');
+                var amountDice = int.Parse(values[0]);
+                var amountSides = int.Parse(values[1]);
+                dice.AddRange(Enumerable.Repeat(new Die(amountSides), amountDice));
+            }
+        }
+        return new DiceRollContext(dice, modifiers);
     }
 }
