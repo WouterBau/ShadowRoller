baf9c9eaa42d6a8e3f89289448331260cb44e440
Wouter Bauchart <wouter.bauchart@outlook.com>
2023-05-02 01:32:37 +0200

    fix: Add parameterless ctor to InvalidAmountSidesException

--- src/ShadowRoller.Domain/InvalidAmountSidesException.cs
+++ src/ShadowRoller.Domain/InvalidAmountSidesException.cs
@@ -4,6 +4,11 @@ namespace ShadowRoller.Domain;
 [Serializable]
 public class InvalidAmountSidesException : ArgumentException
 {
+    public InvalidAmountSidesException()
+    {
+        
+    }
+
     protected InvalidAmountSidesException(SerializationInfo serializationInfo, StreamingContext streamingContext)
         : base(serializationInfo, streamingContext)
     {
