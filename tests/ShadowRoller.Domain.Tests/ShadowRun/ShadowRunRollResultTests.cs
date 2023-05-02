ed8d613314b028ff8c061f3a589cf9433d7e12d9
Wouter Bauchart <wouter.bauchart@outlook.com>
2023-05-02 23:56:11 +0200

    chore: Added ShadowRunRollResult unit tests to increase coverage

--- tests/ShadowRoller.Domain.Tests/ShadowRun/ShadowRunRollResultTests.cs
+++ tests/ShadowRoller.Domain.Tests/ShadowRun/ShadowRunRollResultTests.cs
@@ -20,7 +20,29 @@ public class ShadowRunRollResultTests
                 0,
                 1,
                 true,
-                true
+                true,
+                "Player 1",
+                @"Player 1 Rolled: 1 Limit: 
+Net amount hits: 0 Amount misses: 1
+CRITICAL GLITCH!!
+"
+            },
+            new object[]
+            {
+                new ShadowRunRollResult
+                {
+                    DiceResults = new []{ 1, 1, 5 }
+                },
+                1,
+                1,
+                2,
+                true,
+                false,
+                "Player 1",
+                @"Player 1 Rolled: 1 1 5 Limit: 
+Net amount hits: 1 Amount misses: 2
+REGULAR GLITCH!!
+"
             },
             new object[]
             {
@@ -32,7 +54,11 @@ public class ShadowRunRollResultTests
                 2,
                 1,
                 false,
-                false
+                false,
+                "Player 1",
+                @"Player 1 Rolled: 1 5 6 Limit: 
+Net amount hits: 2 Amount misses: 1
+"
             },
             new object[]
             {
@@ -45,19 +71,41 @@ public class ShadowRunRollResultTests
                 1,
                 1,
                 false,
-                false
+                false,
+                "Player 1",
+                @"Player 1 Rolled: 1 5 6 5 Limit: 1
+Net amount hits: 1 Amount misses: 1
+"
             },
         };
     }
 
     [Theory]
     [MemberData(nameof(ResultTestsValues))]
-    public void ResultTests(ShadowRunRollResult resultItem, int expectedGrossHits, int expectedNetHits, int expectedMisses, bool expectedHasGlitched, bool expectedGlitchedCriticall)
+    public void ResultTests(ShadowRunRollResult resultItem,
+        int expectedGrossHits, int expectedNetHits, int expectedMisses,
+        bool expectedHasGlitched, bool expectedGlitchedCriticall,
+        string player, string expectedOutput)
     {
         Assert.Equal(expectedGrossHits, resultItem.GrossAmountHits);
         Assert.Equal(expectedNetHits, resultItem.NetAmountHits);
         Assert.Equal(expectedMisses, resultItem.AmountMisses);
         Assert.Equal(expectedHasGlitched, resultItem.HasGlitched);
         Assert.Equal(expectedGlitchedCriticall, resultItem.HasGlitchedCritically);
+        Assert.Equal(expectedOutput, resultItem.ToString(player));
+    }
+
+    [Theory]
+    [InlineData(new[] { 0, 1 }, new[] { 1 })]
+    [InlineData(new[] { 6, 7 }, new[] { 6 })]
+    [InlineData(new[] { 0, 1, 2, 3, 4, 5, 6, 7 }, new[] { 1, 2, 3, 4, 5, 6 })]
+    [InlineData(null, new int[] { })]
+    public void ShadowRunRollResult_Filters_Invalid_Values(int[] inputDiceResults, int[] expectedDiceResults)
+    {
+        var result = new ShadowRunRollResult()
+        {
+            DiceResults = inputDiceResults
+        };
+        Assert.Equivalent(expectedDiceResults, result.DiceResults);
     }
 }
